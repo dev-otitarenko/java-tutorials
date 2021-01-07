8703e7acc4eae5b8ec85a848b9d0a68f3982caa4
oleksii titarenko <atitarenko.eng@gmail.com>
2021-01-07 19:08:53 +0100

    ua-common_utils: init

--- ua-common-utils/src/main/java/ua/maestro/lib/commons/Utils.java
+++ ua-common-utils/src/main/java/ua/maestro/lib/commons/Utils.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ua.maestro.lib.commons;
 
 import ua.maestro.lib.commons.utils.StringUtil;
 import org.apache.commons.lang3.StringUtils;
 
-/**
- *
- * @author maestro
- */
 public class Utils {
     public static boolean isEmpty(String str) {
         return StringUtil.isEmpty(str);
