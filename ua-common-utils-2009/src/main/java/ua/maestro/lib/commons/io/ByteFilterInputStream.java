4c92183f86859d8b88e51690b37858c16feff31c
dev-otitarenko <76594207+dev-otitarenko@users.noreply.github.com>
2021-01-07 20:07:46 +0100

    Develop (#3)
    * added css-parser lib
    
    * project
    
    * project
    
    * added junit5-lib-gson
    
    * ua-common-utils: init
    
    * ua-tax-xml-utils: init
    
    * ua-tax-xml-utils: init
    
    * ua-common_utils: init
    
    * ua-taxtemplates-utils: init
    
    * project
    
    * project
    
    * project

---
+++ ua-common-utils-2009/src/main/java/ua/maestro/lib/commons/io/ByteFilterInputStream.java
@@ -0,0 +1,50 @@
+package ua.maestro.lib.commons.io;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * {@link FilterInputStream} that filters bytes.  
+ */
+public class ByteFilterInputStream extends FilterInputStream {
+
+	private final byte[] filterBytes;
+	
+	/**
+	 * @param in {@link InputStream} that should be filtered.
+	 * @param filterBytes The bytes which should be filtered. Each single byte in the array will match to the filter.
+	 */
+	public ByteFilterInputStream(final InputStream in, final byte[] filterBytes) {
+		super(in);
+		this.filterBytes = filterBytes;
+	}
+
+	@Override
+	public int read(byte[] buf, int off, int len) throws IOException {
+		int read = super.read(buf, off, len);
+		int readResult = read;
+		if(read > 0) {
+			int o = 0;
+			for(int i = 0 ; i < read; i++) {
+				byte b = buf[i];
+				if(isFilter(b)) {
+					readResult--;
+					continue;
+				}
+				buf[o] = buf[i];
+				o++;
+			}
+		}
+		return readResult;
+	}
+
+	private boolean isFilter(byte b) { 
+		for(byte filterbyte : filterBytes) {
+			if(filterbyte == b) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ ua-common-utils-2009/src/test/java/ua/maestro/lib/commons/collection/IteratorListTest.java
@@ -0,0 +1,20 @@
+package ua.maestro.lib.commons.collection;
+
+import java.util.ArrayList;
+import junit.framework.TestCase;
+
+public class IteratorListTest extends TestCase {
+
+	public void testCursor() {
+		ArrayList<String> first = new ArrayList<>();
+		first.add("1");
+		first.add("2");
+		first.add("3");
+		first.add("4");
+		
+		IteratorList<String> t = new IteratorList<>(first.iterator(), first.size());
+		System.out.println(t.get(1));
+		System.out.println(t.get(3));
+		System.out.println(t.get(0));
+	}
+}
